60928d45e9644b7a2245bd3d5e974460f533a4cb
Alex <aconstantin86@yahoo.com>
2023-03-01 21:47:58 +0200

    finished

--- ORM/Program.cs
+++ ORM/Program.cs
@@ -10,10 +10,10 @@ internal class Program
         using (var db = new DatabaseContext())
         {
             Console.WriteLine("All Assocs");
-            ReadAllAssocs(db);
+            //ReadAllAssocs(db);
 
             Console.WriteLine("All Customers");
-            ReadAllCustomers(db);
+            //ReadAllCustomers(db);
 
             Console.WriteLine("All orders");
             //ReadAllOrders(db);
@@ -23,6 +23,21 @@ internal class Program
             
             Console.WriteLine("all products");
             //ReadAllProducts(db);
+
+            Console.WriteLine("All customers in only one assoc");
+            //ReadAllCustomersWithOnlyOneAssoc(db);
+
+            Console.WriteLine("All assocs with only one customer");
+            //ReadAllAssocsWithOnlyOneCust(db);
+
+            Console.WriteLine("find a customer by id");
+            //FindCustomerById(db,GetARandomCustomer(db).CustomerId);
+
+            Console.WriteLine("find an assoc by id");
+            //FindAssociationById(db,GetARandomAssociation(db).AssociationId);
+
+            Console.WriteLine("find a product by id");
+            //FindProductById(db,GetARandomProduct(db).ProductId);
 
             Console.WriteLine("Add a customer");
             //AddCustomer(db,GenerateRandomCustomer(db));
@@ -34,15 +49,19 @@ internal class Program
             //AddOrderItem(db,GenerateRandomOrderItem(db));
 
             Console.WriteLine("Add an association");
-            //AddAssociation(db,GenerateRandomAssociation(db));
+            //AddAssociation(db,GenerateRandomAssociation());
 
             Console.WriteLine("Add a product");
-            //AddProduct(db,GenerateRandomProduct(db));
+            //AddProduct(db,GenerateRandomProduct());
 
             Console.WriteLine("add a customer to a assoc");
             //AddCustomerToAssoc(db,GetARandomCustomer(db).CustomerId, GetARandomAssociation(db).AssociationId);
 
-            
+            Console.WriteLine("total orders from an assoc");
+            //GetTotalOrdersForAssociation(db,GetARandomAssociation(db).AssociationId);
+
+            Console.WriteLine("Total orders from an assoc with only loyal customers");
+            //GetTotalOrdersForAssociationWithLoyalCustomer(db,GetARandomAssociation(db).AssociationId);
 
         }
     }
---
+++ ORM/readme.md
@@ -0,0 +1,3 @@
+# Am adaugat in Program.cs majoritatea functiilor pe care le-am facut
+# fiecare cu un Console.WriteLine() pentru a scre ce fac ( sunt destul de descriptive oricum ca si nume)
+# si comentate methodele, pentru a nu lua foc calculatorul la rulare
